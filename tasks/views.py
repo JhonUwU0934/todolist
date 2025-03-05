98e1f6edebe11c0ee66fcf26de2d92ad366f621a
AlejoLopeda <lopedadaniel>
2025-03-05 18:47:41 -0500

    home tareas v0.11

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 # Create your views here.
 
+class UserLoginView(LoginView):
+    template_name = 'tasks/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('index')
+    
+    
 def index(request):
     tasks = pending_tasks.objects.all()
     context = {'tasks': tasks}

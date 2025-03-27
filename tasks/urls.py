9a4f8a0af2bd043a344038fefc6277794170d13e
AlejoLopeda <lopedadaniel>
2025-03-27 16:37:47 -0500

    registeruser v0.1

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path 
 from . import views
-from .views import UserLoginView
+from .views import UserLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name= 'login'),
     path('logout/', LogoutView.as_view(next_page= 'login'), name= 'logout'),
     path('', views.index, name="index"),
+    path('register/',RegisterPage.as_view(), name='register')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from .models import *
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
+from django.views.generic.edit import FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 # Create your views here.
 
 class UserLoginView(LoginView):
@@ -12,7 +16,24 @@ class UserLoginView(LoginView):
     
     def get_success_url(self):
         return reverse_lazy('index')
-    
+        
+class RegisterPage(FormView):
+    template_name = 'tasks/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('index')
+        return super(RegisterPage, self).get(*args, **kwargs)
+  
     
 def index(request):
     tasks = pending_tasks.objects.all()

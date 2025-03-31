73726aa5ac756b9e6c06ea9a303c61a908b19038
AlejoLopeda <lopedadaniel>
2025-03-30 22:43:40 -0500

    home tareas v.0.3

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView
+
 
 # Create your models here.
 
@@ -9,6 +11,10 @@ class pending_tasks(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     done_task = models.BooleanField(default=False)
+    
+    def form_invalid(self, form):  #Conecta la información del usuario
+        form.instance.user = self.request.user
+        return super (pending_tasks, self).form_valid(form)
 
     def  __str__(self):
         return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import *
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
+from django.views.generic.list import ListView
 from django.views.generic.edit import FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
@@ -33,6 +34,19 @@ class RegisterPage(FormView):
         if self.request.user.is_authenticated:
             return redirect('index')
         return super(RegisterPage, self).get(*args, **kwargs)
+
+#LoginRequiredMixin para restringir paginas/tareas por usuario
+class TaskList(LoginRequiredMixin, ListView):
+    model = pending_tasks #En Admin tiene se llama pending_taskss 
+    context_object_name = 'pending_tasks'
+    
+    #Conecta las tareas según el usurio que las creo, es decir, solo las deja visibles a cierto usurio dueño de la tarea
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pending_tasks'] = context['pending_tasks'].filter(user=self.request.user)
+        context['count'] = context['pending_tasks'].filter(complete=False).count()
+        return context
+  
   
     
 def index(request):
